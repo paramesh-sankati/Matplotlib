4014783462b21b2fdc6ceca540c155985cca1ff2
paramesh-sankati <parameshyadav865@gmail.com>
2025-03-01 10:13:16 +0530

    matplotlib intro

---
+++ matplotIntro.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot()
+plt.xlabel("x-axis")  #label name to x axis
+plt.ylabel("y-axis")    #label name to y axis
+plt.title("Student Performance Evaluation")  #graph title
+plt.show()
